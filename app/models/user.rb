b1667469e5ad0d8fbca437bf85fcc82bdd43e12c
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-12 16:39:18 +0000

    add navigation bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 30 }
+  # validates :name, presence: true, length: { maximum: 30 }
 
   has_many :transactions, class_name: "Transaction", foreign_key: "transaction_id"
   has_many :groups, class_name: "Group", foreign_key: "group_id"
