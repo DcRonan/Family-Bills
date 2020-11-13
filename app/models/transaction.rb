6483302870ee40fd83b12faac1b34d69eaadbbab
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-13 09:11:46 +0000

    add name to devise and migration - working

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
-  has_and_belongs_to_many :user, foreign_key: 'author_id'
-  has_many :groups, class_name: 'Group', foreign_key: 'group_id'
+  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :groups, class_name: 'Group', foreign_key: 'group_id'
 end
